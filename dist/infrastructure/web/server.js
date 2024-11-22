9d01c537393bebc1d6bc9c471dd45255e0bf7ab3
samurogo <samrodri29140@gmail.com>
2024-11-22 00:48:44 -0600

    Cambios Nuevos

--- dist/infrastructure/web/server.js
+++ dist/infrastructure/web/server.js
@@ -5,13 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.startServer = void 0;
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const sensorRoutes_1 = __importDefault(require("./routes/sensorRoutes"));
 const sensorECGRoutes_1 = __importDefault(require("./routes/sensorECGRoutes"));
 const sensorMAX30100Routes_1 = __importDefault(require("./routes/sensorMAX30100Routes"));
 const sensorGPSRoutes_1 = __importDefault(require("./routes/sensorGPSRoutes"));
+const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const startServer = () => {
     const app = (0, express_1.default)();
+    app.use((0, cors_1.default)({
+        origin: ['http://localhost:3000', 'http://example.com'],
+    }));
     app.use(express_1.default.json());
+    app.use('/api/auth', authRoutes_1.default);
     app.use('/api', sensorRoutes_1.default);
     app.use('/api', sensorECGRoutes_1.default);
     app.use('/api', sensorMAX30100Routes_1.default);
--- src/infrastructure/web/server.ts
+++ src/infrastructure/web/server.ts
@@ -1,14 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 import sensorRoutes from './routes/sensorRoutes';
 import sensorECGRoutes from './routes/sensorECGRoutes';
 import sensorMAX30100Routes from './routes/sensorMAX30100Routes';
 import sensorGPSRoutes from './routes/sensorGPSRoutes';
+import authRoutes from './routes/authRoutes';
 
 export const startServer = () => {
   const app = express();
 
+  app.use(cors({
+    origin: ['http://localhost:3000', 'http://example.com'],
+  }));
+
   app.use(express.json());
 
+  app.use('/api/auth', authRoutes);
   app.use('/api', sensorRoutes);
   app.use('/api', sensorECGRoutes);
   app.use('/api', sensorMAX30100Routes);
